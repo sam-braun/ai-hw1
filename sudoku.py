5b2a43957a9c40ee85951f1e48498647ee5249e7
Samuel Braun <slb2250@columbia.edu>
2023-10-18 11:50:05 -0400

    minor changes to build_csp

--- sudoku.py
+++ sudoku.py
@@ -42,30 +42,6 @@ def select_unassigned_variable(csp):
         return None
     return min_domain_tile
 
-
-def tile_check(board, tile, value):
-    tile_row_coord, tile_col_coord = tile[0], tile[1]
-    tile_col = [row + tile_col_coord for row in ROW]
-    tile_row = [tile_row_coord + col for col in COL]
-
-    # check column and row
-    if value in tile_col or value in tile_row:
-        return False
-
-    # check mini box
-    col_idx, row_idx = COL.index(tile_col), ROW.index(tile_row)
-    box_col, box_row = col_idx // 3, row_idx // 3
-
-    box_cols = [COL[box_col * 3], COL[(box_col * 3) + 1], COL[(box_col * 3) + 2]]
-    box_rows = [ROW[box_row * 3], ROW[(box_row * 3) + 1], ROW[(box_row * 3) + 2]]
-
-    for rows in box_cols:
-        for cols in box_rows:
-            if board[rows + cols] == value:
-                return False
-
-    return True
-
 def board_check(board):
     # iterate through columns
     for index in ROW:
@@ -151,10 +127,15 @@ def backtracking_helper(board, csp):
         csp_copy = csp.copy()
         new_csp = forward_check(csp_copy, tiny, value)
 
-        if new_csp:
-            board[tiny] = value
-
+        if new_csp is None:
+            return None
         
+        board[tiny] = value
+        result = backtracking_helper(board, new_csp)
+        if result:
+            return result
+    
+    return None
 
     """
     function BACKTRACK(assignment, sp) returns a solution, or failure
@@ -172,36 +153,29 @@ def backtracking_helper(board, csp):
         return failure
     """
 
-    if board_check(board):
-        return board
-    
-    var = select_unassigned_value(board)
-    if var is None:
-        return None
-
-    for value in range(1, 10): # ORDER_DOMAIN_VALUES??????????
-        if tile_check(board, var, value):
-            board[var] = value
-            result = backtracking(board)
-            if result:
-                return result
-            board[var] = 0
-    
-    return None
-
 def build_csp(board):
     csp = {}
     for col in COL:
         for row in ROW:
             if board[row + col] == 0:
                 csp.update({row+col:[1,2,3,4,5,6,7,8,9]})
+            else:
+                csp.update({row+col:board[row+col]})
     
     for row in ROW:
         for col in COL:
-            elements = {elem for elem in row}
-            elements.update({elem for elem in col})
-            elements = list(elements)
-            csp.update({row+col:[elem for elem in csp[row+col] if elem not in elements]})
+            elements = set()
+            for c in COL:
+                elements.add(board[row+c])
+            for r in ROW:
+                elements.add(board[r+col])
+            if 0 in elements:
+                elements.add(0)
+            
+            print(csp[row+col])
+
+            elements, curr_vals = list(elements), csp[row+col]
+            csp.update({row+col:[elem for elem in curr_vals if elem not in elements]})
 
     if csp:
         return csp
@@ -210,8 +184,8 @@ def build_csp(board):
 def backtracking(board):
     csp = build_csp(board)
     if csp:
-        backtracking_helper(board, csp)
-    return board
+        final_board = backtracking_helper(board, csp)
+    return final_board
 
 
 if __name__ == '__main__':
@@ -271,80 +245,3 @@ if __name__ == '__main__':
             outfile.write('\n')
 
         print("Finishing all boards in file.")
-
-
-
-"""
-def is_complete(board):
-    Check if the board is completely filled.
-    return all(value != 0 for value in board.values())
-
-    
-
-
-def select_unassigned_variable(board):
-    Return the variable with minimum remaining values using MRV heuristic.
-    mrv = None
-    for tile in board:
-        if board[tile] == 0:
-            possible_values = sum(1 for value in range(1, 10) if is_consistent(tile, value, board))
-            if mrv is None or possible_values < mrv[1]:
-                mrv = (tile, possible_values)
-    return mrv[0] if mrv else None
-
-    
-
-
-
-
-
-
-def is_consistent(tile, value, board):
-    Check if the value is consistent with the board.
-    row, col = tile[0], tile[1]
-    # Check row, column, and box
-    return all([
-        value not in [board[row + c] for c in COL],
-        value not in [board[r + col] for r in ROW],
-        value not in [board[ROW[(ord(row) - 65) // 3 * 3 + r] + COL[(int(col) - 1) // 3 * 3 + c]] for r in range(3) for c in range(3)]
-    ])
-
-def inferences(board, tile, value):
-    Perform forward checking and return inferences.
-    inferences = {}
-    for t in board:
-        if board[t] == 0 and is_consistent(t, value, board):
-            inferences[t] = 0
-    return inferences
-
-def backtracking(board):
-    Takes a board and returns solved board using backtracking with MRV and forward checking.
-    if is_complete(board):
-        return board
-
-    var = select_unassigned_variable(board)
-    if var is None:
-        return None  # No unassigned variable found
-
-    for value in range(1, 10):
-        if is_consistent(var, value, board):
-            board[var] = value
-            infer = inferences(board, var, value)
-            
-            if 0 not in infer.values():  # If inferences do not lead to failure
-                board.update(infer)
-                result = backtracking(board)
-                if result:
-                    return result
-                board[var] = 0
-                for t in infer:
-                    board[t] = 0  # Remove inferences
-
-    return None  # Failure
-
-
-
-
-
-
-"""
